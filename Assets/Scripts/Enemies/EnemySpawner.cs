ac7afec908490404efbe5c1c74018ead46704a05
Eefschmeef2310 <ethan.lucasawesome@gmail.com>
2022-12-17 17:12:14 +1100

    Enemy Spawn rate now tied to curve

--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -5,32 +5,23 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     public List<GameObject> enemyList;
-    public float enemySpawnIntervalMax = 7f;
-    public float enemySpawnIntervalMin = 1f;
-    public float enemySpawnIntervalModifier = -0.1f;
-
+    public AnimationCurve curve;
     private float spawnTimer = 0f;
-    private float currentInterval;
 
-    // Start is called before the first frame update
     void Start()
     {
-        spawnTimer = enemySpawnIntervalMax;
-        currentInterval = enemySpawnIntervalMax;
+        Invoke("spawn", curve.Evaluate(spawnTimer));
+    }
+
+    void spawn()
+    {
+        Instantiate(enemyList[Random.Range(0, enemyList.Count)]);
+        Invoke("spawn", curve.Evaluate(spawnTimer));
     }
 
     // Update is called once per frame
     void Update()
     {
-        spawnTimer -= Time.deltaTime;
-        if (spawnTimer <= 0f)
-        {
-            // Spawn an enemy
-            Instantiate(enemyList[Random.Range(0, enemyList.Count)]);
-
-            // Reset the timer
-            currentInterval = Mathf.Max(currentInterval + enemySpawnIntervalModifier, enemySpawnIntervalMin);
-            spawnTimer = currentInterval;
-        }
+        spawnTimer += Time.deltaTime;
     }
 }
