5dc3902c824ae2e0822b6b61b834d2f30e70d2bc
Cookie <x.akkari@outlook.com>
2022-12-17 16:28:19 +1100

    implemented shark and random enemy spawns
    also fixed the spawn timer going up instead of down

---
+++ Assets/Scripts/Enemies/SharkController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharkController : MonoBehaviour
+{
+    public float windup = 1f;
+    public GameObject warningPrefab;
+    private GameObject warningObject;
+
+    public GameObject hitboxPrefab;
+    private GameObject hitboxObject;
+
+    private float lifetimer = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Go to player coords
+        Vector3 playerPos = FindObjectOfType<PlayerController>().transform.position;
+        transform.position = new Vector3(playerPos.x, playerPos.y);
+
+        // Spawn warning
+        warningObject = Instantiate(warningPrefab, transform.position, Quaternion.identity, transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (windup > 0f)
+        {
+            windup -= Time.deltaTime;
+        }
+        else
+        {
+            if (warningObject != null)
+            {
+                // Attack
+                Destroy(warningObject);
+                hitboxObject = Instantiate(hitboxPrefab, transform.position, Quaternion.identity, transform);
+            }
+            else if (hitboxObject != null && lifetimer < 0.9f)
+            {
+                Destroy(hitboxObject);
+            }
+
+            lifetimer -= Time.deltaTime;
+            if (lifetimer <= 0f)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
