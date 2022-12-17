9ff4f1ede64f73fc44aa7115656bde12fd8e3eeb
Cookie <x.akkari@outlook.com>
2022-12-17 16:35:09 +1100

    Fixed crab controller

--- Assets/Scripts/Enemies/CrabController.cs
+++ Assets/Scripts/Enemies/CrabController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class CrabController : MonoBehaviour
 {
-    public float windup = 0.7f;
     public float speed = 20f;
     Vector3 endPos;
     GameObject sea;
+
+    public float windup = 0.7f;
+    public GameObject warningPrefab;
+    private GameObject warningObject;
 
     void Start()
     {
@@ -17,24 +20,40 @@ public class CrabController : MonoBehaviour
         switch (Random.Range(0, 2)) //randomly pick a side, then spawn a crab at a random spot along that axis
         {
             case 0: //left
-                position = new Vector3(-Camera.main.orthographicSize * Camera.main.aspect, Random.Range(-Camera.main.orthographicSize, sea.transform.position.y));
+                position = new Vector3(-Camera.main.orthographicSize * Camera.main.aspect - 1, Random.Range(-Camera.main.orthographicSize, sea.transform.position.y));
                 break;
             case 1: //right
-                position = new Vector3(Camera.main.orthographicSize * Camera.main.aspect, Random.Range(-Camera.main.orthographicSize, sea.transform.position.y));
+                position = new Vector3(Camera.main.orthographicSize * Camera.main.aspect + 1, Random.Range(-Camera.main.orthographicSize, sea.transform.position.y));
                 break;
         }
         transform.position = position;
         endPos = new Vector3(-position.x, position.y);
+
+        // Spawn warning
+        warningObject = Instantiate(warningPrefab, new Vector3(transform.position.x - (Mathf.Sign(transform.position.x) * 3), transform.position.y), Quaternion.identity, transform);
+
     }
     void Update()
     {
-        if (transform.position.Equals(endPos))
+        if (windup > 0f)
         {
-            Destroy(gameObject);
+            windup -= Time.deltaTime;
         }
         else
         {
-            transform.position = Vector3.MoveTowards(transform.position, endPos, speed * Time.deltaTime);
+            if (warningObject != null)
+            {
+                Destroy(warningObject);
+            }
+
+            if (transform.position.Equals(endPos))
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, endPos, speed * Time.deltaTime);
+            }
         }
     }
 }
