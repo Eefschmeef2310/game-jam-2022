9834dbd027b8bdd3aa9d0b0f8d53fbc165c2be54
Cookie <x.akkari@outlook.com>
2022-12-17 21:17:01 +1100

    Trash animation

--- Assets/Scripts/Trash/TrashMovement.cs
+++ Assets/Scripts/Trash/TrashMovement.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class TrashMovement : MonoBehaviour
 {
-    public float speed = 1f;
+    public float speed = 3f;
+    public List<Sprite> spriteList;
+
+    private void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = spriteList[Random.Range(0, spriteList.Count)];
+    }
 
     // Update is called once per frame
     void Update()
