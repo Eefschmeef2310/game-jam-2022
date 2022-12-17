1cc9a4eba5544d649da7a5c4d0ee5b587b8d8197
Cookie <x.akkari@outlook.com>
2022-12-17 14:20:45 +1100

    player movement now exists

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float movementSpeed = 2f;
     public Rigidbody2D rb;
-    Vector2 movementDirection;
+    public float maxSpeed = 2f;
+
+    public float acceleration = 50f;
+    public float friction = 1f;
+
+    private Vector2 movementDirection;
+    private Vector2 inputDirection;
+
     void Update()
     {
-        movementDirection = Vector3.ClampMagnitude(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")),1);
+        // Input direction will always be a maximum value
+        inputDirection = Vector3.ClampMagnitude(new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")), 1);
+
+        // We control actual direction from here
+        if (inputDirection.Equals(Vector2.zero))
+        {
+            // Not moving; slow down to 0
+            movementDirection = Vector2.Lerp(movementDirection, Vector2.zero, friction * Time.deltaTime);
+        }
+        else
+        {
+            // Moving; speed up to input direction
+            movementDirection = Vector2.Lerp(movementDirection, inputDirection, acceleration * Time.deltaTime);
+        }
     }
     void FixedUpdate()
     {
-        rb.velocity = movementDirection * movementSpeed;
+        rb.velocity = movementDirection * maxSpeed;
     }
+
 }
