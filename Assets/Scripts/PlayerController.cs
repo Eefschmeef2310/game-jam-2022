51834708bf918e6c89923fb91737971e883d18ba
Cookie <x.akkari@outlook.com>
2022-12-17 14:18:49 +1100

    Revert "player movement works as intended"
    This reverts commit 1a6fbd75a586e1922336b771df48f5c4dda34890.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,35 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float movementSpeed = 2f;
     public Rigidbody2D rb;
-    public float maxSpeed = 2f;
-
-    public float acceleration = 50f;
-    public float friction = 1f;
-
-    private Vector2 movementDirection;
-    private Vector2 inputDirection;
-
+    Vector2 movementDirection;
     void Update()
     {
-        // Input direction will always be a maximum value
-        inputDirection = Vector3.ClampMagnitude(new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")), 1);
-
-        // We control actual direction from here
-        if (inputDirection.Equals(Vector2.zero))
-        {
-            // Not moving; slow down to 0
-            movementDirection = Vector2.Lerp(movementDirection, Vector2.zero, friction * Time.deltaTime);
-        }
-        else
-        {
-            // Moving; speed up to input direction
-            movementDirection = Vector2.Lerp(movementDirection, inputDirection, acceleration * Time.deltaTime);
-        }
+        movementDirection = Vector3.ClampMagnitude(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")),1);
     }
     void FixedUpdate()
     {
-        rb.velocity = movementDirection * maxSpeed;
+        rb.velocity = movementDirection * movementSpeed;
     }
-
 }
